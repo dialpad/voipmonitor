2a32ad8d6cef1b2e9ea5d64c8e7ffcc328d468a2
Carlos Montoya-Dulche <cmontoya-dulche@dialpad.com>
2019-12-13 10:34:54 -0800

    Set permissions to pid file.

--- voipmonitor.cpp
+++ voipmonitor.cpp
@@ -2158,6 +2158,11 @@ static void daemonize(void)
 		if (f) {
 		       fprintf(f, "%ld\n", (long)vmon_pid);
 		       fclose(f);
+               // # Making pid file writable only to owner (TEL-10545)
+               int result = chmod(opt_pidfile, S_IROTH|S_IWUSR);
+               if(restult != 0){
+                   syslog(LOG_WARNING,"Permissions for pid file %s could not be set [Error: %d]\n", opt_pidfile, result);
+               }  
 		} else {
 		       syslog(LOG_ERR,"Error occurs while writing pid file to %s\n", opt_pidfile);
 		}
