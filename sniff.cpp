50dea25057546b4569a14dd4de538980e5c3865b
Rajesh Pathak <rajesh.pathak@dialpad.com>
2021-08-23 16:43:43 +0530

    Add to hash for video

--- sniff.cpp
+++ sniff.cpp
@@ -3220,6 +3220,22 @@ void process_sdp(Call *call, packet_s_process *packetS, int iscaller, char *from
 									       sdp_media_data_item->srtp_crypto_config_list, sdp_media_data_item->srtp_fingerprint,
 									       to, branch, iscaller, sdp_media_data_item->rtpmap, sdp_media_data_item->sdp_flags);
 						}
+						//m=video support
+						if (sdp_media_data_item->sdp_flags.is_video())
+						{
+							call->add_ip_port_hash(packetS->saddr_(), sdp_media_data_item->ip, ip_port_call_info::_ta_base_video, sdp_media_data_item->port, packetS->getTimeval_pt(),
+								       sessid, sdp_media_data_item->label, sdp_media_data_count > 1,
+								       sdp_media_data_item->srtp_crypto_config_list, sdp_media_data_item->srtp_fingerprint,
+								       to, branch, iscaller, sdp_media_data_item->rtpmap, sdp_media_data_item->sdp_flags);
+							// check if the IP address is listed in nat_aliases
+							vmIP alias = match_nat_aliases(sdp_media_data_item->ip);
+							if(alias.isSet()) {
+								syslog(LOG_ERR, "[%s] nat_aliases is not supported for video", call->fbasename);
+							}
+							if(opt_sdp_reverse_ipport) {
+								syslog(LOG_ERR, "[%s] sdp_reverse_ipport is not supported for video", call->fbasename);
+							}
+						}
 					}
 				}
 			} else if(!sdp_media_data_item->ip.isSet()) {
@@ -5003,7 +5019,8 @@ inline int process_packet__rtp_call_info(packet_s_process_calls_info *call_info,
 		iscaller = call_info->calls[call_info_index].iscaller;
 		sdp_flags = call_info->calls[call_info_index].sdp_flags;
 		is_rtcp = call_info->calls[call_info_index].is_rtcp || 
-			  ((sdp_flags.is_audio() || sdp_flags.is_video()) && packetS->datalen_() > 1 && RTP::isRTCP_enforce(packetS->data_()));
+			  ((sdp_flags.is_audio() || sdp_flags.is_video()) && packetS->datalen_() > 1 &&
+			  ((u_char)packetS->data_()[1] == 0xC8 || (u_char)packetS->data_()[1] == 0xC9) && RTP::isRTCP_enforce(packetS->data_()));
 		stream_in_multiple_calls = call_info->calls[call_info_index].multiple_calls;
 		
 		if(!call_info->find_by_dest && iscaller_is_set(iscaller)) {
