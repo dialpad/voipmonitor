6f60f6bdde185ba346f16c6254118b0c1cd18777
Rajesh Pathak <rajesh.pathak@dialpad.com>
2021-09-01 12:07:50 +0530

    Adding support for VP8

--- sniff.cpp
+++ sniff.cpp
@@ -1811,7 +1811,10 @@ int get_rtpmap_from_sdp(char *sdp_text, unsigned long len, bool is_video, RTPMAP
 		if (sscanf(s, "%30u %254[^/]/%d", &payload, mimeSubtype, &rate) == 3) {
 			// store payload type and its codec into one integer with 1000 offset
 			if(is_video) {
-				codec = PAYLOAD_VIDEO;
+			  if (mimeSubtypeToInt(mimeSubtype) == PAYLOAD_VP8)
+			    codec = PAYLOAD_VP8;
+			  else
+					codec = PAYLOAD_VIDEO;
 			} else {
 				codec = mimeSubtypeToInt(mimeSubtype);
 				if(codec == PAYLOAD_G7221) {
@@ -3219,6 +3222,22 @@ void process_sdp(Call *call, packet_s_process *packetS, int iscaller, char *from
 									       sessid, sdp_media_data_item->label, sdp_media_data_count > 1, 
 									       sdp_media_data_item->srtp_crypto_config_list, sdp_media_data_item->srtp_fingerprint,
 									       to, branch, iscaller, sdp_media_data_item->rtpmap, sdp_media_data_item->sdp_flags);
+						}
+												//m=video support
+						if (sdp_media_data_item->sdp_flags.is_video())
+						{
+							call->add_ip_port_hash(packetS->saddr_(), sdp_media_data_item->ip, ip_port_call_info::_ta_base_video, sdp_media_data_item->port, packetS->getTimeval_pt(),
+								       sessid, sdp_media_data_item->label, sdp_media_data_count > 1,
+								       sdp_media_data_item->srtp_crypto_config_list, sdp_media_data_item->srtp_fingerprint,
+								       to, branch, iscaller, sdp_media_data_item->rtpmap, sdp_media_data_item->sdp_flags);
+							// check if the IP address is listed in nat_aliases
+							vmIP alias = match_nat_aliases(sdp_media_data_item->ip);
+							if(alias.isSet()) {
+								syslog(LOG_ERR, "[%s] nat_aliases is not supported for video", call->fbasename);
+							}
+							if(opt_sdp_reverse_ipport) {
+								syslog(LOG_ERR, "[%s] sdp_reverse_ipport is not supported for video", call->fbasename);
+							}
 						}
 					}
 				}
